ef031f3b8de329cab66a2959cc448664882ff9fd
MKemp <mkemp@accu-time.com>
2020-04-15 15:43:58 -0400

    Retrofit interface, client, and data object for GitHubRepo

---
+++ app/src/main/java/com/mkemp/githubrxjava/retrofit/GitHubService.java
@@ -0,0 +1,15 @@
+package com.mkemp.githubrxjava.retrofit;
+
+import com.mkemp.githubrxjava.data.GitHubRepo;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface GitHubService
+{
+    @GET("users/{user}/starred")
+    Observable<List<GitHubRepo>> getStarredRepositories(@Path("user") String userName);
+}
